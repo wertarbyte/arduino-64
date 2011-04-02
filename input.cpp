7989d2c5b4cd66cf37fc75f7533c4562a60cc32a
Stefan Tomanek <stefan@pico.ruhr.de>
2011-04-02 21:22:23 +0200

    add second push button

--- input.cpp
+++ input.cpp
@@ -1,9 +1,10 @@
 #include "WProgram.h"
 #include "input.h"
 
-#define BTN_CNT 1
+#define BTN_CNT 2
 static struct { uint8_t pin; boolean state; } button[BTN_CNT] = {
 	{ 7, LOW },
+	{ 2, LOW },
 };
 
 #define POT_CNT 1
